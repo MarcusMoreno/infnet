5cf43b7bb1b15bc6b73a4bbe9c23ad39ee4aed22
Marcus Moreno <moreno.mvv@gmail.com>
2017-06-28 08:24:26 -0300

    Adiciona metodo no Migrations/Configuration

--- ProjetoIndividualPersistencia/Persistencia/Migration/Configuration.cs
+++ ProjetoIndividualPersistencia/Persistencia/Migration/Configuration.cs
@@ -9,5 +9,25 @@ namespace ProjetoIndividual.Persistencia.Migration
 {
     internal sealed class Configuration : DbMigrationsConfiguration<ProjetoIndividual.Persistencia.Context.PJContext>
     {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProjetoIndividual.Persistencia.Context.PJContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
     }
 }
