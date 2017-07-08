d1722b36702d226401d663077e7817bbdb8750c0
Marcus Moreno <moreno.mvv@gmail.com>
2017-07-08 17:59:45 -0300

    Cria projeto do console

---
+++ ProjetoIndividualPersistencia/Dominio/Entidades/Fornecedor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIndividual.Dominio
+{
+    public class Fornecedor
+    {
+        public Fornecedor()
+        {
+            Enderecos = new List<Endereco>();
+            Produtos = new List<Produto>();
+            Processos = new List<Processo>();
+        }
+
+        public string Cnpj { get; set; }
+
+        public string RazaoSocial { get; set; }
+
+        public string InscricaoMunicipal { get; set; }
+
+        public Decimal ReceitaBruta { get; set; }
+
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+        
+        public virtual ICollection<Produto> Produtos { get; set; }
+
+        public virtual ICollection<Processo> Processos { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ ProjetoIndividualPersistencia/ProjetoIndividual.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoIndividual.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
